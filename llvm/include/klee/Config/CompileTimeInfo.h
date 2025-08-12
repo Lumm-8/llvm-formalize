187c256375edf3929a2064bc01b82dac58ffacc3
Lumm-8 <2980857248@qq.com>
2025-08-12 14:09:29 +0800

    Add header files of klee and buddy

---
+++ llvm/include/klee/Config/CompileTimeInfo.h
@@ -0,0 +1,17 @@
+//===-- CompileTimeInfo.h ---------------------------------------*- C++ -*-===//
+//
+//                     The KLEE Symbolic Virtual Machine
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// AUTOMATICALLY GENERATED. DO NOT EDIT!
+#ifndef KLEE_COMPILETIMEINFO_H
+#define KLEE_COMPILETIMEINFO_H
+
+#define KLEE_BUILD_MODE "RelWithDebInfo (Asserts: ON)"
+#define KLEE_BUILD_REVISION "7444f8e8b9751ad9c7e427d054d80b7db33b43ee"
+
+#endif /* KLEE_COMPILETIMEINFO_H */
